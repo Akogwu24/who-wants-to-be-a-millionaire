564ebe534a45023c383fcf4a84bdf745017a8441
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-12-28 13:42:54 +0100

    questions gradient added

--- src/components/main/components/Question.jsx
+++ src/components/main/components/Question.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 
 const Question = () => {
   return (
-    <Square border='2px solid #aaa' p='20px' borderRadius='10px'>
+    <Square
+      border='2px solid #aaa'
+      p='20px'
+      bg='linear-gradient(#22074d, black)'
+      borderRadius='10px'
+    >
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sit
       incidunt dignissimos reprehenderit nostrum voluptatum, omnis nam, eos
-      cupiditate recusandae fuga vitae ad, velit corporis est ipsam corrupti
-      ipsa a!
     </Square>
   );
 };
